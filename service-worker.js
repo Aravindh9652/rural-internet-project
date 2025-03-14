a2b2b0f052615738024e71ca1e5484cd04d5b549
Aravindh9652 <vajjaaravindh@gmail.com>
2025-03-15 00:49:49 +0530

    Updated UI, added PWA & dark mode toggle

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,38 @@
-const CACHE_NAME = "rural-internet-cache";
-const urlsToCache = [
-    "/",
-    "/index.html",
-    "/styles.css",
-    "/script.js",
-    "/manifest.json"
+const CACHE_NAME = "rural-internet-v2";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/styles.css",
+  "/script.js",
+  "/manifest.json",
+  "/icon.png"
 ];
 
+// Install Service Worker
 self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(urlsToCache))
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
+  );
 });
 
+// Fetch Cached Files
 self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => response || fetch(event.request))
-    );
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Clean Up Old Caches
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
 });
